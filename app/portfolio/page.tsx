59aba2d0ec7c7d3ad710450cc761a7472409f45d
that devguy <zkmutch@gmail.com>
2024-07-15 18:59:40 -0400

    updated portfolio h3 font sizes

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -52,7 +52,7 @@ const Portfolio = () => {
             commitment to excellence.
           </p>
           <div className="mr-auto flex flex-col pt-10 lg:pt-24">
-            <h3 className="title mr-auto text-2xl font-bold uppercase italic text-brand-primary sm:text-2xl lg:text-4xl 2xl:text-5xl">
+            <h3 className="title mr-auto text-2xl font-bold uppercase italic text-brand-primary sm:text-2xl lg:text-3xl 2xl:text-3xl">
               Kitchen Remodels
             </h3>
             <div className="mr-auto flex h-full select-none items-center">
@@ -63,7 +63,7 @@ const Portfolio = () => {
       </section>
       <section className="flex flex-col items-center bg-slate-100">
         <div className="flex w-full max-w-[1920px] flex-col items-center overflow-hidden p-4 py-10 sm:px-10 lg:pt-12">
-          <h3 className="title mr-auto text-2xl font-bold uppercase italic text-brand-primary sm:text-2xl lg:text-4xl 2xl:text-5xl">
+          <h3 className="title mr-auto text-2xl font-bold uppercase italic text-brand-primary sm:text-2xl lg:text-3xl 2xl:text-3xl">
             Bathroom Remodels
           </h3>
           <div className="mr-auto flex h-full select-none items-center">
@@ -73,7 +73,7 @@ const Portfolio = () => {
       </section>
       <section className="flex flex-col items-center bg-white">
         <div className="flex w-full max-w-[1920px] flex-col items-center overflow-hidden p-4 py-10 sm:px-10 lg:pt-12">
-          <h3 className="title mr-auto text-2xl font-bold uppercase italic text-brand-primary sm:text-2xl lg:text-4xl 2xl:text-5xl">
+          <h3 className="title mr-auto text-2xl font-bold uppercase italic text-brand-primary sm:text-2xl lg:text-3xl 2xl:text-3xl">
             Home Renovations
           </h3>
           <div className="mr-auto flex h-full select-none items-center">
@@ -83,7 +83,7 @@ const Portfolio = () => {
       </section>
       <section className="flex flex-col items-center bg-slate-100">
         <div className="flex w-full max-w-[1920px] flex-col items-center overflow-hidden p-4 py-10 sm:px-10 lg:pt-12">
-          <h3 className="title mr-auto text-2xl font-bold uppercase italic text-brand-primary sm:text-2xl lg:text-4xl 2xl:text-5xl">
+          <h3 className="title mr-auto text-2xl font-bold uppercase italic text-brand-primary sm:text-2xl lg:text-3xl 2xl:text-3xl">
             Interior Painting
           </h3>
           <div className="mr-auto flex h-full select-none items-center">
