7c6262108871c368d68abcfa674f3059a579fe4f
that_devguy <zkmutch@gmail.com>
2024-07-29 14:31:21 -0400

    working on fix for safari zoom on form

--- components/ui/form.tsx
+++ components/ui/form.tsx
@@ -133,7 +133,7 @@ const FormDescription = React.forwardRef<
     <p
       ref={ref}
       id={formDescriptionId}
-      className={cn("text-sm text-neutral-500 dark:text-neutral-400", className)}
+      className={cn("text-base text-neutral-500 dark:text-neutral-400", className)}
       {...props}
     />
   )
@@ -155,7 +155,7 @@ const FormMessage = React.forwardRef<
     <p
       ref={ref}
       id={formMessageId}
-      className={cn("text-sm font-medium text-red-500 dark:text-red-900", className)}
+      className={cn("text-base font-medium text-red-500 dark:text-red-900", className)}
       {...props}
     >
       {body}
--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -9,7 +9,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
     return (
       <input
         type={type}
-        className={cn("flex h-10 w-full rounded-md border border-neutral-200 bg-white px-3 py-2 text-sm outline-none focus:ring-2 focus:border-neutral-200",
+        className={cn("flex h-10 w-full rounded-md border border-neutral-200 bg-white px-3 py-2 text-base outline-none focus:ring-2 focus:border-neutral-200",
           
           className
         )}
